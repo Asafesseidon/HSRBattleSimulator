21184d6f463ca5e1cd9cf021f5a675cab1e6b6a1
Asafe Domingues Marcelino <66213067+Asafesseidon@users.noreply.github.com>
2024-08-20 11:55:20 -0300

    Add files via upload

---
+++ Kit.js
@@ -0,0 +1,80 @@
+function Skill(id){
+
+    if(id == 1){
+
+    }
+
+    else if(id == 2){
+        
+    }
+}
+
+function BasicAttack(id){
+
+    if(id == 1){
+
+    }
+
+    else if(id == 2){
+
+    }
+}
+
+function Talent(id){
+
+    if(id == 1){
+
+    }
+
+    else if(id == 2){
+
+    }
+}
+
+function Ultimate(id){
+
+    if(id == 1){
+
+    }
+
+    else if(id == 2){
+
+    }
+}
+
+function FuA(id){
+
+    if (id.triggerFuACondition){
+
+        var triggerFuA = 2;
+
+    }
+
+    
+    var counterFuA = 1;
+    var stackFuA = 3;
+
+    if(id == 1){
+
+    }
+
+    else if(id == 2){
+
+    }
+}
+
+function DoT(id){
+
+    var burn = 1;
+    var bleed = 2;
+    var windShear = 3;
+    var shock = 4;
+
+    if(id == 1){
+
+    }
+
+    else if(id == 2){
+
+    }
+}

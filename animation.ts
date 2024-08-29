b801056d3b90e88c42f4b8296e134733d03defd8
Asafe Domingues Marcelino <66213067+Asafesseidon@users.noreply.github.com>
2024-08-29 00:18:00 -0300

    Update animation.ts

--- animation.ts
+++ animation.ts
@@ -1,9 +1,53 @@
-class ultimate{
-    energy:number;
-    multiplier:number;
-    actual_energy:number;
-    constructor(energy:number,multiplier:number){
+class BasicAttack{
+
+    private name:string;
+    private multiplier:number;
+    private spGain:number;
+
+    constructor(name:string, multiplier:number, spGain:number) {
+
+        this.name = name;
+        this.multiplier = multiplier;
+        this.spGain = spGain;
+    }
+}
+
+class Skill{
+
+    private name:string;
+    private type:string;//Either an ATK or a Buff
+    private multiplier:number;
+    private spUsage:number;
+
+    constructor(name:string, type:string, multiplier:number, spUsage:number) {
+
+        this.name = name;
+        this.type = type;
+        this.multiplier = multiplier;
+        this.spUsage = spUsage;
+    }
+}
+
+class FuA{
+
+    private name:string;
+    private type: string;
+    private condition: string;
+    private multiplier: number;
+}
+
+class Ultimate{
+
+    private name:string;
+    private energy:number;
+    private type:string;
+    private multiplier:number;
+    private actual_energy:number;
+    constructor(name:string,energy:number, type:string, multiplier:number){
+        
+        this.name = name;
         this.energy=energy;
+        this.type = type;
         this.multiplier=multiplier;
         this.actual_energy = 0;
     }
@@ -19,7 +63,7 @@ class ultimate{
     }
 
 }
-class litghcone{
+class LightCone{
     private vida:number;
     private attack:number;
     private defense: number;
@@ -30,38 +74,111 @@ class litghcone{
         this.vida=life;
     }
 }
-class personagem{
+class Personagem{
     private id:string
-    private vida:number;
+    private life:number;
     private attack:number;
     private defense: number;
     private speed: number;
-    private weapon:litghcone;
-    constructor(life:number,weapon:litghcone, attack:number, defense: number, speed:number,id:string){
+    private weapon:LightCone;
+    private basicAttack: BasicAttack;
+    private skill:Skill;
+    constructor(life:number,weapon:LightCone, basicAttack: BasicAttack, skill:Skill, attack:number, defense: number, speed:number,id:string){
         this.id=id
-        this.vida=life;
+        this.life=life;
         this.weapon=weapon;
+        this.basicAttack = basicAttack;
+        this.skill = skill;
         this.attack=attack;
         this.defense=defense;
         this.speed=speed;
     }
     public setlife(value:number){
-        this.vida+=value;
+        this.life+=value;
     }
     public takelife():number{
-        return this.vida;
+        return this.life;
     }
 }
 
-var ally1=new personagem(2600,new litghcone(800,500,450),1000,800,100,'hpAlly1')
-var ally2=new personagem(2600,new litghcone(800,500,450),1000,800,100,'hpAlly2')
-var ally3=new personagem(2600,new litghcone(800,500,450),1000,800,100,'hpAlly3')
-var ally4=new personagem(2600,new litghcone(800,500,450),1000,800,100,'hpAlly4')
-var ally5=new personagem(2600,new litghcone(800,500,450),1000,800,100,'hpAlly5')
-var ally6=new personagem(2600,new litghcone(800,500,450),1000,800,100,'hpAlly6')
+class Enemy extends Personagem{
+
+    toughness:number
+
+    constructor(toughness:number, life:number,weapon:LightCone, basicAttack:BasicAttack, skill:Skill, attack:number, defense: number, speed:number,id:string){
+        super(life,weapon,basicAttack, skill, attack, defense, speed, id);
+
+        this.toughness=toughness;
+    }
+}
+function chars(id, iteration){
+
+    if (iteration >= 4){
+
+        iteration = 4;
+    } 
+
+    if (id == 0) {
+
+        var ally = [];
+        iteration++;
+
+        return ally;
+    }
+
+    else if (id == 1) {
+
+        var ally = [new Personagem(2600,new LightCone(800,500,450),new BasicAttack('a', 50, 1), new Skill('a', 'attack', 200, 2 ), 1000,800,100,'hpAlly' + iteration)];//Huohuo
+        iteration++;
+
+        return ally;
+    }
+
+    else if (id == 2) {
+
+        var ally = [new Personagem(2600,new LightCone(800,500,450),new BasicAttack('a', 50, 1), new Skill('a', 'attack', 200, 2 ),1000,800,100,'hpAlly' + iteration)]; //Kafka
+        iteration++;
+
+        return ally;
+    }
+
+    else if (id == 3) {
+
+        var ally= [new Personagem(2600,new LightCone(800,500,450),new BasicAttack('a', 50, 1), new Skill('a', 'attack', 200, 2 ), 1000,800,100,'hpAlly' + iteration)]; //Black Swan
+        
+        return ally;
+    }
+
+    else if (id == 4) {
+
+        var ally= [new Personagem(2600,new LightCone(800,500,450),new BasicAttack('a', 50, 1), new Skill('a', 'attack', 200, 2 ), 1000,800,100,'hpAlly' + iteration)]; //Ruan Mei
+        iteration++;
+
+        return ally;
+    }
+
+    else if (id == 5) {
+
+        var ally= [ new Personagem(2600,new LightCone(800,500,450),new BasicAttack('a', 50, 1), new Skill('a', 'attack', 200, 2 ), 1000,800,100,'hpAlly' + iteration)];//Aventurine
+        iteration++;
+
+        return ally;        
+    }
+
+    else if (id == 6) {
+
+        var ally = [new Personagem(2600,new LightCone(800,500,450),new BasicAttack('a', 50, 1), new Skill('a', 'attack', 200, 2 ), 1000,800,100,'hpAlly1')]; //Yanqing
+        iteration++;
+        return ally;
+    }
+
+    
+    return iteration;
+
+}
 
 function animacao_da_vida_1(hpId, dano, vida) {
-    var larguraAtual = vida * 6;  // Supondo que a largura seja diretamente proporcional � vida
+    var larguraAtual = Personagem[].takelife() * 6;  // Supondo que a largura seja diretamente proporcional � vida
     var interval = setInterval(() => {
         larguraAtual -= 1;
         if (larguraAtual <= (vida - dano) * 6) {
@@ -74,9 +191,6 @@ function animacao_da_vida_1(hpId, dano, vida) {
     setTimeout(() => {
         if (vida-dano<=0){
             var normal=document.getElementById(hpId).innerHTML;
-            document.getElementById(hpId).innerHTML+='<br><h1>charizard ganhou</h1>'
-            vidap2=100;
-            vidap1=100
             document.getElementById(hpId).style.width ='600px';
             setTimeout(() => {
                 document.getElementById(hpId).innerHTML=normal
@@ -86,7 +200,7 @@ function animacao_da_vida_1(hpId, dano, vida) {
 }
 
 function ultAnimation(ultimate) {
-    var altura = personagem.TakeEnergy()/20;  // Supondo que a largura seja diretamente proporcional � vida
+    var altura = Ultimate[].TakeEnergy()/20;  // Supondo que a largura seja diretamente proporcional � vida
     var interval = setInterval(() => {
         altura -= 1/20;
         if (altura <= (vida + dano) * 6) {

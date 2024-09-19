a3e207e41ed9c2c6b9f44a2a487d066f2ae0f87e
Asafe Domingues Marcelino <66213067+Asafesseidon@users.noreply.github.com>
2024-09-19 11:29:34 -0300

    Create form_db.js

---
+++ form_db.js
@@ -0,0 +1,76 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var nomearquivo = "." + q.pathname;
+    console.log("Nome do arquivo: " + nomearquivo);
+
+    if(nomearquivo == "./"){
+      fs.readFile("index.html", function(err, data) {
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Arquivo não encontrado!");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+    }
+    else if(nomearquivo == "./formulario.html"){
+      fs.readFile(nomearquivo, function(err, data) {
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Arquivo não encontrado!");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+          
+    }
+
+    else if(nomearquivo == "./registra.html"){
+      let nome = q.query.nome;
+      let email = q.query.email;
+      let senha = q.query.senha;
+
+      let db = new sqlite3.Database('./db/banco.db', (err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log('Conectou com o banco de dados!');
+      });
+
+      // insere um registro no banco de dados
+      db.run(`INSERT INTO usuario(nome, email, senha) VALUES(?,?,?)`, ['Clécio','clecio@teste.com','123'], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`Registro feito com sucesso no id ${this.lastID}`);
+      });
+
+      db.close((err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log('Fechou a conexão com o banco de dados!');
+      });
+    }
+
+    else{
+    //console.log("Nome: " + q.nome + " E-mail: " + q.email + " Senha: " + q.senha);
+      fs.readFile(nomearquivo, function(err, data) {
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Arquivo não encontrado!");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+    }
+}).listen(8080, () => {
+    console.log("O servidor foi iniciado na porta 8080");
+});
